2afdef278bd7cbab0189909590ecbd240bce324e
jkind <jose.kindelan@peoplewalking.com>
2023-11-10 17:23:12 -0500

    initial commit

---
+++ WebApp/Program.cs
@@ -0,0 +1,43 @@
+// var builder = WebApplication.CreateBuilder(args);
+//
+// // Add services to the container.
+//
+// builder.Services.AddControllers();
+// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+//
+// var app = builder.Build();
+//
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+//
+// app.UseHttpsRedirection();
+//
+// app.UseAuthorization();
+//
+// app.MapControllers();
+//
+// app.Run();
+
+namespace WebApp;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+        // await host.CreateDbIfNotExists<RebusPayDbContext>();
+        await host.RunAsync();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+
+    
+}
---
+++ WebApp/Startup.cs
@@ -0,0 +1,65 @@
+﻿namespace WebApp;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+    public IWebHostEnvironment Env { get; }
+
+    public Startup(IConfiguration configuration, IWebHostEnvironment env)
+    {
+        Configuration = configuration;
+        Env = env;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Controllers
+        //
+        services.AddControllers();
+
+        // SPA
+        //
+        // services.AddSpaStaticFiles(configuration => configuration.RootPath = "ClientApp/build");
+
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        // Force HTTS
+        //
+        app.UseHttpsRedirection();
+
+        // Register the Swagger generator and the Swagger UI middlewares
+        //
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        // Routing
+        //
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+
+            endpoints.Map("/api/{**slug}", context =>
+            {
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+                return Task.CompletedTask;
+            });
+        });
+        
+        // app.UseStaticFiles();
+        // app.UseSpaStaticFiles();
+        // app.UseSpa(spa =>
+        // {
+        //     spa.Options.SourcePath = "ClientApp";
+        //
+        //     if (Env.IsDevelopment())
+        //     {
+        //         spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");
+        //     }
+        // });
+    }
+}
